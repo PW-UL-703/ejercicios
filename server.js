003b39d25778352cd73170f40e7dda584bd9d805
edhu <edgar.huaranga@uni.pe>
2023-11-08 18:09:25 -0500

    server inicial

--- server.js
+++ server.js
@@ -5,17 +5,10 @@ const path = require('path');
 const app = express()
 const port = 3080
 
-
-const estudiante = require('./api/estudiante')
-const profesor = require('./api/profesor')
-
 /* Esta es la parte del Middleware */
 app.use(express.static(path.join(__dirname, './static')));
 app.use(bodyParser.json());
 
-
-app.use('/api/xyz/', estudiante);
-app.use('/api/', profesor);
 
 app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname, './static/index.html'));
